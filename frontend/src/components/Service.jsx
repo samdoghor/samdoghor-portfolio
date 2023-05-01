9ad5d2da7c51015b94d522ecc3fbc51e7e1ea597
unknown <samdoghor@gmail.com>
2023-05-01 16:42:31 +0100

    header and intro

--- frontend/src/components/Service.jsx
+++ frontend/src/components/Service.jsx
@@ -5,7 +5,7 @@ const Service = () => {
   return (
     <>
       <div className="w-100 h-100" id="services">
-        <p className="text-center text-2xl text-white text-bold pb-10">
+        <p className="text-center text-2xl text-white text-bold pb-12">
           Still Under Construction
         </p>
       </div>
