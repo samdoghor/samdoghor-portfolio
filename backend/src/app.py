f5f175504ff69eea6ee4fcac8293f35c0fc48c5e
Samuel Doghor <56834362+samdoghor@users.noreply.github.com>
2023-05-24 03:24:46 +0100

    fix: CORS

--- backend/src/app.py
+++ backend/src/app.py
@@ -14,7 +14,7 @@ import config
 # configurations
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+cors = CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://samdoghor-portfolio-backend.onrender.com"]}})  # noqa: E501
 app.config['SECRET_KEY'] = config.SECRET_KEY
 
 app.debug = config.DEBUG
